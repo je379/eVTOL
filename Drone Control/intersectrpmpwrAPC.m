0855b45c8b36fa8d51f79ba23eecda2124f5ffdd
Jordan Eriksen <je379@cam.ac.uk>
2020-02-05 17:14:12 +0000

    Calibrate APC power and RPM

---
+++ Drone Control/intersectrpmpwrAPC.m
@@ -0,0 +1,23 @@
+load('EXP_DATA_PROP.mat');
+load('APC_FIT.mat', 'APC_FIT');
+%%
+rpmlim = [3000 12000];
+
+testrpm = linspace(rpmlim(1), rpmlim(2), 10000);
+testpwr = APC_FIT.rpm_pwr(testrpm);
+
+for i = 1:length(APC.P)
+    apcpwr = ones([1, 10000]) .* APC.P(i);
+    diff = apcpwr - testpwr;
+    for j = 1:10000
+        diffj = diff(j);
+        if diffj < 0
+            break
+        end
+    end
+    rpm(i) = testrpm(j);
+end
+
+APC.rpmmean = rpm;
+
+save('EXP_DATA_PROP.mat', 'APC', 'BASE');
